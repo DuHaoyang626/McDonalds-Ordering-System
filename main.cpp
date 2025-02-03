7be6f91814c82227995739080e081382763afc83
duhaoyang626 <duhaoyang626@gmail.com>
2025-02-04 00:03:48 +0800

    Qt test V2 commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <QCoreApplication>
-#include <QDebug>
+#include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
-    return QCoreApplication::exec();
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
 }
